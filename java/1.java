13ab3ee223b6bb6f132f301c89a93e20dab59404
moulz07 <moulikakreddy@gmail.com>
2024-12-22 13:06:08 +0530

    first commit

---
+++ java/1.java
@@ -0,0 +1,16 @@
+class EvenNumber
+{
+    public static void main (String args[])
+    {
+        int i=40;
+        while(i<=80)
+        {
+            if (i%2==0)
+            {
+                System.out.println(i);
+            }
+        i++;
+        }
+        while(i<=80);
+    }
+}
---
+++ javascript/1.js
@@ -0,0 +1,17 @@
+const accountId=144552
+let accountEmail="moulikakreddy@gmail.com"
+var accountPassword="12345"
+accountCity ="bangalore"
+let accountState;
+
+
+//accountId=2//not alowed
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
